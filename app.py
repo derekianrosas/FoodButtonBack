75239ca0d546b1df2cc7ad4e0e9e7124aae9b837
derek rosas <derekianrosas@gmail.com>
2020-06-22 19:32:18 -0600

    figuring out image data stuff

--- app.py
+++ app.py
@@ -8,7 +8,27 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_imageattach.entity import Image, image_attachment
 from sqlalchemy_imageattach.context import store_context
+import mysql.connector
+from mysql.connector import Error
 
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+
+def insertBLOB(emp_id, name, photo, biodataFile):
+    print("Inserting BLOB into python_employee table")
+    try:
+        connection = mysql.connector.connect(host='ec2-34-192-173-173.compute-1.amazonaws.com',
+        database='d9eg2abehv5ppe',
+        user='nyjifrotlfgwbj',
+        password='4d427557ea3920e6669a7fa76c058f7ab853c5e1559c430a45330ad0a17f479c')
+
+        cursor = connection.cursor()
+        sql_insert_blob_query = 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://nyjifrotlfgwbj:4d427557ea3920e6669a7fa76c058f7ab853c5e1559c430a45330ad0a17f479c@ec2-34-192-173-173.compute-1.amazonaws.com:5432/d9eg2abehv5ppe"
@@ -24,6 +44,8 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(Unicode, nullable=False)
     picture = image_attachment('UserFoodPicture')
+
+
 
 class UserFoodPicture(Base, Image):
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
